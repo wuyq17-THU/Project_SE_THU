2c9cb6f624309934e8236e6fd982296f91c259e8
Leonidas9826 <49238205+Leonidas9826@users.noreply.github.com>
2020-11-15 11:25:12 +0800

    前端1.0.0 完成head

---
+++ 前端/code/front/src/components/Head.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <div id="head">
+        <h1>叮咚</h1>
+        <p>清华大学工会场馆预约系统</p>
+    </div>
+    <h1>{{ msg }}</h1>
+    <p>点击下方登录进入，如已经登录请点击上方home链接</p>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Head',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+#head{
+    background-color: blueviolet;
+    width: 1440px;
+    height: 100px;
+}
+</style>
